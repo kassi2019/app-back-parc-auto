4414827b29dc7634d89cb84ae4fea4a5e0731ef8
kassi2019 <57103350+kassi2019@users.noreply.github.com>
2025-11-07 09:47:06 +0000

    creation des controlleur et services1

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,4 +23,4 @@ import { VehiculesModule } from './vehicules/vehicules.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
